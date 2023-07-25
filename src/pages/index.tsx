f12dddb1255b93d742c4c73b687e9b04be3f95bc
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-07-25 14:07:16 +0530

    Initial Commit `0.1.0`

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TSX</title>
+        <meta
+          name="description"
+          content="Personal portfolio of software engineer cum full stack web developer."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="font-semibold lg:text-[1.4rem]">TSX is under construction!</h1>
+      </main>
+    </>
+  );
+}
