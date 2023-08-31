d305b670d9f843cc8c04244549f62765c48934b6
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-08-31 22:04:47 +0530

    feat: New API Route (`Blogs`), Customized Skeleton Loader, Blogs Ready ✅

--- src/components/home/BlogArea.tsx
+++ src/components/home/BlogArea.tsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
-import BlogCard from "../common/BlogCard";
 
-import data from 'public/data.json';
+import BlogCard from "../common/BlogCard";
+import Loader from "../common/SkeletonLoader";
+import type ClickableItemProps from "~/interfaces/clickableItem";
+import type HashnodeResponse from "~/interfaces/hashnodeResponse";
+
 
 export default function BlogArea() {
   const theme = useTheme();
+  const [blogs, setBlogs] = useState<ClickableItemProps[]>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const resp = await fetch('/api/blogs', {
+        method: 'GET',
+      });
+      const hashnodeResp = await resp.json() as HashnodeResponse;
+      const blogs = hashnodeResp.data.user.publication.posts.map((post) => ({
+        title: post.title,
+        description: post.brief,
+        url: `https://tanmaysarkar.hashnode.com/${post.slug}`,
+      }));
+      setBlogs(blogs);
+      setLoading(false);
+    }
+
+    void fetchBlogs();
+  }, [])
 
   return (
-
     <Box sx={{
       gap: '1rem',
       display: 'flex',
@@ -28,9 +51,16 @@ export default function BlogArea() {
           backgroundColor: "rgba(0,0,0,0)",
         },
       }}>📝 BLOGS</Typography>
-      {data.BlogDetails.map((bd, i) => (
-        <BlogCard key={i} props={{ ...bd, timeOut: i * 300 }} />
-      ))}
+      {loading && <Loader w="100%" h={60} v='rectangular' sx={{ borderRadius: theme.custom.clickableItem.borderRadius }} />}
+      {!loading && blogs?.map((bd, i) => (<BlogCard key={i} props={{ timeOut: i * 300, ...bd }} />))}
     </Box>
   );
-} 
+}
+
+
+export function getServerSideProps() {
+
+  return {
+    props: {}
+  };
+}
